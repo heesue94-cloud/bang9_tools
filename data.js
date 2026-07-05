93e2ea09d2ae93888be1fe8b1243785d3a27951f
heesue94-cloud <heesue94@gmail.com>
2026-07-05 18:58:19 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,224 @@
+const USERS = [
 
+    {
+        id: "bang9",
+        name: "방구",
+        color: "#EF4444",
+
+        characters: [
+
+            {
+                id: "bang9_1",
+                owner: "bang9",
+
+                job: "보마",
+                level: 200,
+
+                role: "dealer",
+
+                attack: 1.31
+            },
+
+            {
+                id: "bang9_2",
+                owner: "bang9",
+
+                job: "보마",
+                level: 200,
+
+                role: "dealer",
+
+                attack: 1.05
+            },
+
+            {
+                id: "bang9_3",
+                owner: "bang9",
+
+                job: "듀블",
+                level: 200,
+
+                role: "dealer",
+
+                attack: 1.45
+            }
+
+        ]
+    },
+
+
+
+
+
+    {
+        id: "lassom",
+        name: "라썸",
+        color: "#8B5CF6",
+
+        characters: [
+
+            {
+                id: "lassom_1",
+                owner: "lassom",
+
+                job: "아란",
+                memo: "메2",
+
+                level: 200,
+
+                role: "dealer",
+
+                attack: 1.52
+            },
+
+            {
+                id: "lassom_2",
+                owner: "lassom",
+
+                job: "보마",
+
+                level: 200,
+
+                role: "dealer",
+
+                attack: 1.32
+            },
+
+            {
+                id: "lassom_3",
+                owner: "lassom",
+
+                job: "캡틴",
+
+                level: 200,
+
+                role: "dealer",
+
+                attack: 0.95
+            }
+
+        ]
+    },
+
+
+
+
+
+    {
+        id: "seunghyun",
+        name: "승현",
+        color: "#22C55E",
+
+        characters: [
+
+            {
+                id: "seunghyun_1",
+                owner: "seunghyun",
+
+                job: "듀블",
+                memo: "메2",
+
+                level: 200,
+
+                role: "dealer",
+
+                attack: 1.50
+            },
+
+            {
+                id: "seunghyun_2",
+                owner: "seunghyun",
+
+                job: "듀블",
+                memo: "메2",
+
+                level: 200,
+
+                role: "dealer",
+
+                attack: 1.50
+            },
+
+            {
+                id: "seunghyun_3",
+                owner: "seunghyun",
+
+                job: "나로",
+
+                level: 200,
+
+                role: "dealer",
+
+                attack: 0.80
+            }
+
+        ]
+    },
+
+
+
+
+
+    {
+        id: "zzonkku",
+        name: "쫀쿠",
+        color: "#F59E0B",
+
+        characters: [
+
+            {
+                id: "zzonkku_1",
+                owner: "zzonkku",
+
+                job: "아란",
+
+                level: 200,
+
+                role: "dealer",
+
+                attack: 1.81
+            },
+
+            {
+                id: "zzonkku_2",
+                owner: "zzonkku",
+
+                job: "닼나",
+
+                level: 200,
+
+                role: "dealer",
+
+                attack: 1.72
+            },
+
+            {
+                id: "zzonkku_3",
+                owner: "zzonkku",
+
+                job: "아란",
+
+                level: 200,
+
+                role: "dealer",
+
+                attack: 1.53
+            },
+
+            {
+                id: "zzonkku_4",
+                owner: "zzonkku",
+
+                job: "듀블",
+
+                level: 200,
+
+                role: "dealer",
+
+                attack: 1.46
+            }
+
+        ]
+    }
+
+];
