3167eb4de14f2fedb4c5a9a755db899dbf644422
heesue94-cloud <heesue94@gmail.com>
2026-07-05 18:59:16 +0900

    Update drag.js

--- drag.js
+++ drag.js
@@ -1,1 +1,75 @@
+function initDrag() {
 
+    // 사용자 목록
+    document.querySelectorAll(".character-list").forEach(list => {
+
+        new Sortable(list, {
+
+            group: "party",
+
+            animation: 180,
+
+            ghostClass: "sortable-ghost",
+
+            chosenClass: "sortable-chosen",
+
+            dragClass: "sortable-drag"
+
+        });
+
+    });
+
+
+    // 파티
+    document.querySelectorAll(".party-body").forEach(list => {
+
+        new Sortable(list, {
+
+            group: "party",
+
+            animation: 180,
+
+            ghostClass: "sortable-ghost",
+
+            chosenClass: "sortable-chosen",
+
+            dragClass: "sortable-drag",
+
+            onAdd: function (evt) {
+
+                checkOwner(evt);
+
+            }
+
+        });
+
+    });
+
+}
+
+
+
+/* ========================================= */
+
+function checkOwner(evt) {
+
+    const card = evt.item;
+
+    const owner = card.dataset.owner;
+
+    const target = evt.to;
+
+    // 파티로 이동은 항상 가능
+    if (target.classList.contains("party-body")) return;
+
+
+    // 사용자 목록으로 이동
+    const targetOwner = target.dataset.owner;
+
+    if (owner === targetOwner) return;
+
+
+    // 다른 사람 목록이면 원래 파티로 되돌림
+    evt.from.appendChild(card);
+
+}
